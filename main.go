a8810e67c5394f0f3318dadf3767c49c20a10ad7
Steve McCoy <mccoyst@gmail.com>
2014-06-15 23:11:37 -0400

    Decode current conditions

--- main.go
+++ main.go
@@ -3,10 +3,9 @@
 package main
 
 import (
-	//"encoding/json"
+	"encoding/json"
 	"flag"
 	"fmt"
-	"io"
 	"io/ioutil"
 	"net/http"
 	"os"
@@ -41,5 +40,24 @@ func main() {
 		os.Exit(1)
 	}
 
-	_, err = io.Copy(os.Stdout, resp.Body)
+	j := json.NewDecoder(resp.Body)
+	var weather conds
+	err = j.Decode(&weather)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Failed to parse response: %v\n", err)
+		os.Exit(1)
+	}
+
+	_, err = fmt.Println(weather)
+}
+
+type conds struct{
+	Current current `json:"current_observation"`
+}
+
+type current struct{
+	Weather string `json:"weather"`
+	TempF float64 `json:"temp_f"`
+	TempC float64 `json:"temp_c"`
+	Humidity string `json:"relative_humidity"`
 }
