c47a9ce46c9aa09652ebd8133320c6b92cdd014f
Steve McCoy <mccoyst@gmail.com>
2014-07-14 11:50:51 -0400

    Start tests

--- wear_test.go
+++ wear_test.go
@@ -1,16 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"testing"
+)
 
 func ExampleClothes() {
 	cs := clothes(65)
-	ds := clothes(80)
 	for _, c := range cs {
 		fmt.Println(c)
 	}
-	for _, d := range ds {
-		fmt.Println(d)
-	}
 	//output: [shirt trousers]
-	// [t-shirt trousers]
+}
+
+func TestPossibilities(t *testing.T) {
+	c := possibilities(100)
+	if len(c) != 2 {
+		t.Fatalf("wrong: %v\n", c)
+	}
+
+	
 }
